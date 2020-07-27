075ca5cb6c1eb7739f8f8588212d283e0d978faa
hsondd <doanson10@gmail.com>
2020-07-27 20:26:52 +0700

    jasdjks

---
+++ CPP/test_imic/final/trac_nghiem/2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class A
+{
+    public:
+        A(int n = 0) : m_n(n){}
+    public:
+        virtual int f() const
+            {return m_n;}
+        virtual ~A(){}
+    protected:
+        int m_n;
+};
+
+class B : public A
+{
+    public:
+        B(int n = 0) : A(n) {}
+
+    virtual int f() const{
+        return m_n + 1;
+    }
+};
+
+int main()
+{
+    const A a(1);
+    const B b(3);
+    const A *x[2] = {&a, &b};
+    typedef vector<A> V;
+    V y({a, b});
+    V::const_iterator i = y.begin();
+
+    cout << x[0]->f() << x[1]->f() << i->f() << (i+1)->f() << endl;
+}
